97cb9e267871e060542c456a080a653e7c434c1d
Chris Diamand <chris@diamand.org>
2014-02-07 16:04:21 +0000

    Added outline of parse tree struct.

--- ptree.h
+++ ptree.h
@@ -1,4 +1,21 @@
 #ifndef PTREE_H
 #define PTREE_H
 
+// Parse tree structure
+
+enum pt_t {
+    PT_CLASS_DEF,
+    PT_VAR_DEF,
+    PT_FUNC_DEF
+};
+
+struct pt {
+    enum pt_t       type;
+    union {
+        int         n;
+    } v;
+};
+
+struct pt *pt_class_def(void);
+
 #endif
